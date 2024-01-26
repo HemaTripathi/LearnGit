c4b0154010afda682438c257b3f49b53f4fc61b1
Hema <157723834+HemaTripathi@users.noreply.github.com>
2024-01-26 10:56:59 -0500

    Mulitplication added

--- GitDemo/src/DemoTest.java
+++ GitDemo/src/DemoTest.java
@@ -4,8 +4,11 @@ public class DemoTest {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 20; 
+		
 		int c = a+b;
+		int d = a*b;
 		System.out.println(c);
+		System.out.println(d);
 
 	}
 
